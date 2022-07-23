e7a56cb5c18241601d3997b6a9a3a8a68a206f9d
Oleksandr Timoshchuk <o.timoshuk@gmail.com>
2022-07-23 17:51:42 +0300

    add contact page

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ import Category from "./pages/Category";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import Listing from "./pages/Listing";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
@@ -29,6 +31,11 @@ function App() {
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/create-listing" element={<CreateListing />} />
+          <Route
+            path="/category/:categoryName/:listingId"
+            element={<Listing />}
+          />
+          <Route path="/contact/:landlordId" element={<Contact />} />
         </Routes>
         <Navbar />
       </Router>
